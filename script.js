2816ca526b431afe76d3640bdc3442d04c3f77df
AnneRoki <59899443+AnneRoki@users.noreply.github.com>
2022-01-07 16:07:32 +0800

    a

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function Total() {
     let pA = document.getElementById("age").value;
     let pW = document.getElementById("weight").value;
     let pH = document.getElementById("height").value;
-    let s
-    let r
+    let gen
+    let x
     if (document.getElementById("age").value.length == 0) {
         return
     }
@@ -14,39 +14,39 @@ function Total() {
         return
     }
     if (document.getElementById("male").checked) {
-        s =  Math.round((10*pW)+(6.25*pH)-(5*pA)+5)
+        gen =  Math.round((10*pW)+(6.25*pH)-(5*pA)+5)
     } else if (document.getElementById("female").checked) {
-        s =  Math.round((10*pW)+(6.25*pH)-(5*pA)-161)
+        gen =  Math.round((10*pW)+(6.25*pH)-(5*pA)-161)
     }
     if (document.getElementById("1").selected) {
-        r = s
+        x = gen
     }
     if (document.getElementById("2").selected) {
-        r = s*1.2
+        x = gen*1.2
     }
     if (document.getElementById("3").selected) {
-        r = s*1.35
+        x = gen*1.35
     }
     if (document.getElementById("4").selected) {
-        r = s*1.55
+        x = gen*1.55
     }
     if (document.getElementById("5").selected) {
-        r = s*1.75
+        x = gen*1.75
     }
     if (document.getElementById("6").selected) {
-        r = s*1.95
+        x = gen*1.95
     }
-    document.getElementById("mw").innerHTML = Math.round(r)
-    document.getElementById("totalBox").style = "margin: auto; width: 150px; height: 75px; background-color:white; color:black;"
+    document.getElementById("mw").innerHTML = Math.round(x)
+    document.getElementById("result").style = "margin: auto; width: 150px;padding:2px; height: 75px; background-color:orange; color:black;border-radius:5ch;text-align:center;"
 }
 
 function intakes(){
-    let cal = document.getElementById("calorie").value;
+    let cal = document.getElementById("cal").value;
     let c;
-    if (document.getElementById("calorie")){
+    if (document.getElementById("cal")){
         c = cal
     }
-    document.getElementById("fCal").innerHTML = c- 200;
-    document.getElementById("lBox").style = "margin: auto; width: 150px; height: 75px; background-color:white; color:black;"
+    document.getElementById("totcal").innerHTML =2000 - c;
+    document.getElementById("con").style = " margin: auto;width: 150px; height: 75px; background-color:orange;padding:2px; color:black;border-radius:5ch;text-align:center;"
 
 }
